11badbbd843cf146e3dec9558508035533157da5
Dalmais Maxence <github@maxired.fr>
2014-05-22 16:05:41 +0200

    remove useless debugging output

--- vendor/auto-reload.js
+++ vendor/auto-reload.js
@@ -6,16 +6,12 @@
   
   var interval = setInterval(  function(){
      if(document.readyState === "complete"){
-      console.log("complete");
       clearInterval(interval);
       if ( window.location.href.indexOf('page_y') != -1 ) {
         var match = window.location.search.split('?')[1].split("&")[0].split("=");
-         console.log(" before set scrolling " , match[1] , document.getElementsByTagName("body")[0].scrollTop  );
-        setTimeout(function(){ 
+         setTimeout(function(){ 
           document.getElementsByTagName("body")[0].scrollTop = 0; 
           document.getElementsByTagName("body")[0].scrollTop = +match[1]; 
-          console.log(" after set scrolling " , match[1] , document.getElementsByTagName("body")[0].scrollTop  );} , 10 );
-        
       }
     }
   } , 1);
@@ -31,17 +27,12 @@
   var reloaders = {
     page: function(){
       var page_y = document.getElementsByTagName("body")[0].scrollTop;
-      console.log("next page y " , page_y);
       var old =   window.location.href;
       var next = window.location.origin + window.location.pathname + '?page_y=' +  page_y + window.location.hash;   
-       window.location.href =  next
-       if(next == old){
-        console.log("force realdo" , window.location.href );
-      window.location.reload(true);
-       }else{
-        console.log("no force realdo");
-       }
-       
+      window.location.href =  next
+      if(next == old){
+        window.location.reload(true);
+      }
     },
 
     stylesheet: function(){
